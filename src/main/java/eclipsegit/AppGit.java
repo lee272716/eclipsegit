9c3e885f1f483a1edf2489c6922524f6bc395c96
lee272716 <51709422+lee272716@users.noreply.github.com>
2020-10-04 12:58:03 +0800

    Update AppGit.java
    GitHub修改提交

--- src/main/java/eclipsegit/AppGit.java
+++ src/main/java/eclipsegit/AppGit.java
@@ -3,7 +3,8 @@ package eclipsegit;
 public class AppGit {
 
 	public static void main(String[] args) {
-			System.out.println("第一次从EClipse项目提交到GitHub");
+		System.out.println("第一次从EClipse项目提交到GitHub");
+		System.out.println("Github修改提交");
 	}
 
 }
