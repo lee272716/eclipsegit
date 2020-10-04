afc488f2540f577317d409fef4674cae565e98d9
leeheng <770971042@qq.com>
2020-10-04 12:56:38 +0800

    本地仓库修改提交

--- src/main/java/eclipsegit/AppGit.java
+++ src/main/java/eclipsegit/AppGit.java
@@ -5,6 +5,7 @@ public class AppGit {
 	public static void main(String[] args) {
 		System.out.println("第一次从EClipse项目提交到GitHub");
 		System.out.println("Github修改提交");
+		System.out.println("本地仓库修改提交");
 	}
 
 }
